853ec791d56544e6d64defead884bdf3d62b69e1
Fahmii19 <fhmialfi79@gmail.com>
2024-09-24 13:09:59 +0700

    ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,9 @@
 
 
     {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
-    @vite('resources/css/app.css')
+    {{-- @vite('resources/css/app.css') --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 
     <title>HalloExpress</title>
 </head>
