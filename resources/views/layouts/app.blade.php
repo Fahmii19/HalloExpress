42dd8ee596232316cb3e6e1712f58e8a9e6f6300
Fahmii19 <fhmialfi79@gmail.com>
2024-09-24 12:59:00 +0700

    push

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 
-    <script src="https://cdn.tailwindcss.com"></script>
-    {{-- @vite('resources/css/app.css') --}}
+    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
+    @vite('resources/css/app.css')
 
     <title>HalloExpress</title>
 </head>
